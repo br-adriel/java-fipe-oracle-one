41ab9548c612a52d01aea1111f3e7318ae599e78
Adriel <adriel.fsantos@outlook.com>
2024-04-30 16:54:56 -0300

    feat: Add call to MainCli on main program file

--- src/main/java/br/com/alura/fipe/FipeApplication.java
+++ src/main/java/br/com/alura/fipe/FipeApplication.java
@@ -1,13 +1,20 @@
 package br.com.alura.fipe;
 
+import br.com.alura.fipe.clis.MainCli;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FipeApplication {
+public class FipeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FipeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var cli = new MainCli();
+		cli.fluxoPrincipal();
+	}
 }
