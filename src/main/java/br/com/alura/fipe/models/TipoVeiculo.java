afef56b6aa2678fec6fc7b9fc2dbd2759e6ffeb7
Adriel <adriel.fsantos@outlook.com>
2024-04-29 22:56:13 -0300

    feat: Update TipoVeiculo enum to hold url slug

--- src/main/java/br/com/alura/fipe/models/TipoVeiculo.java
+++ src/main/java/br/com/alura/fipe/models/TipoVeiculo.java
@@ -1,7 +1,17 @@
 package br.com.alura.fipe.models;
 
 public enum TipoVeiculo {
-    CAMINHAO,
-    CARRO,
-    MOTO,
+    CAMINHAO("caminhoes"),
+    CARRO("carros"),
+    MOTO("motos");
+
+    private final String slug;
+
+    TipoVeiculo(String slug) {
+        this.slug = slug;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
 }
