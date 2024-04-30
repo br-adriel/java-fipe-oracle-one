cd5e016324b3c89bdfd579ebed832c63e1937f43
Adriel <adriel.fsantos@outlook.com>
2024-04-30 16:54:03 -0300

    feat: Add FipeApiService

--- src/main/java/br/com/alura/fipe/services/FipeApiService.java
+++ src/main/java/br/com/alura/fipe/services/FipeApiService.java
@@ -1,14 +1,13 @@
 package br.com.alura.fipe.services;
 
-import br.com.alura.fipe.models.DadosMarca;
-import br.com.alura.fipe.models.DadosModelo;
-import br.com.alura.fipe.models.TipoVeiculo;
+import br.com.alura.fipe.models.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FipeApiService extends ConsumoApi {
     private final ObjectMapper mapper = new ObjectMapper();
@@ -37,6 +36,37 @@ public class FipeApiService extends ConsumoApi {
                     }
             );
             return parsedJson.get("modelos");
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<DadosVeiculo> getInformacoesVeiculos(
+            TipoVeiculo tipoVeiculo,
+            DadosMarca marca,
+            DadosModelo modelo
+    ) {
+        var url = BASE_URL + tipoVeiculo.getSlug() + "/marcas/"
+                  + marca.codigo() + "/modelos/" + modelo.codigo() + "/anos";
+        var json = super.obterDados(url);
+        try {
+            var anos = mapper.readValue(
+                    json,
+                    new TypeReference<ArrayList<DadosAnos>>() {
+                    });
+            return anos.stream()
+                    .map(a -> getDadosVeiculo(url + "/" + a.codigo()))
+                    .collect(Collectors.toCollection(ArrayList::new));
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private DadosVeiculo getDadosVeiculo(String url) {
+        var json = super.obterDados(url);
+        try {
+            return mapper.readValue(json, DadosVeiculo.class);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
