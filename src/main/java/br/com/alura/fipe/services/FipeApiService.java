0ee06263346650c60b2da6104425175879feb250
Adriel <adriel.fsantos@outlook.com>
2024-04-30 00:37:21 -0300

    feat: Add getModelos method to FipeApiService

--- src/main/java/br/com/alura/fipe/services/FipeApiService.java
+++ src/main/java/br/com/alura/fipe/services/FipeApiService.java
@@ -1,12 +1,14 @@
 package br.com.alura.fipe.services;
 
 import br.com.alura.fipe.models.DadosMarca;
+import br.com.alura.fipe.models.DadosModelo;
 import br.com.alura.fipe.models.TipoVeiculo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class FipeApiService extends ConsumoApi {
     private final ObjectMapper mapper = new ObjectMapper();
@@ -18,6 +20,23 @@ public class FipeApiService extends ConsumoApi {
             return mapper.readValue(
                     json, new TypeReference<ArrayList<DadosMarca>>() {
                     });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<DadosModelo> getModelos(TipoVeiculo tipoVeiculo, DadosMarca marca) {
+        var json = super.obterDados(
+                BASE_URL + tipoVeiculo.getSlug() + "/marcas/"
+                + marca.codigo() + "/modelos"
+        );
+        try {
+            var parsedJson = mapper.readValue(
+                    json,
+                    new TypeReference<Map<String, ArrayList<DadosModelo>>>() {
+                    }
+            );
+            return parsedJson.get("modelos");
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
